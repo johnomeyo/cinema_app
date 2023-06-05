127806945e69e1512cab19c0625eb83e8f12d5fd
John <85222720+johnomeyo@users.noreply.github.com>
2023-06-05 10:56:25 +0300

    tickets

--- cinema_app/lib/navigation_pages/tickets.dart
+++ cinema_app/lib/navigation_pages/tickets.dart
@@ -10,25 +10,6 @@ class Tickets extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        // child: Consumer<BookSeat>(
-        //   builder: (BuildContext context, bookSeat, Widget? child) {
-        //     return GestureDetector(
-        //       onTap: () {
-        //         bookSeat.bookSeat(3);
-        //       },
-        //       child: Container(
-        //         height: 40,
-        //         width: 40,
-        //         decoration: BoxDecoration(
-        //           borderRadius: BorderRadius.circular(5),
-        //           color: bookSeat.isBooked
-        //               ? Colors.pink.shade100
-        //               : Colors.red.shade600,
-        //         ),
-        //       ),
-        //     );
-        //   },
-        // ),
         child: Text("Tickets"),
       ),
     );
